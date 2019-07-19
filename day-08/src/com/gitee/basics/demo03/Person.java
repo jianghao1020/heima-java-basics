eb031a658040e3e7d5cf06ffb27c1d8e2cdf1b6c
jianghao <jianghao19941020@aliyun.com>
2019-07-19 18:01:28 +0800

    String类、static、Arrays类、Math类

---
+++ day-08/src/com/gitee/basics/demo03/Person.java
@@ -0,0 +1,14 @@
+package com.gitee.basics.demo03;
+
+
+public class Person {
+
+    static {
+        System.out.println("静态代码块执行！");
+    }
+
+    public Person() {
+        System.out.println("构造方法执行！");
+    }
+
+}
