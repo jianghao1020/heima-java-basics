8caa1c1368ac0d41a435fa2ddbe1e56c67510ae0
jianghao <jianghao19941020@aliyun.com>
2019-07-19 17:56:07 +0800

    类与对象、封装、构造方法

---
+++ day-06/src/com/gitee/basics/demo04/Demo01Person.java
@@ -0,0 +1,15 @@
+package com.gitee.basics.demo04;
+
+public class Demo01Person {
+
+    public static void main(String[] args) {
+        Person person = new Person();
+        // 设置我自己的名字
+        person.name = "王健林";
+        person.sayHello("王思聪");
+
+        System.out.println(person); // 地址值
+        System.out.println(person.name);
+    }
+
+}
