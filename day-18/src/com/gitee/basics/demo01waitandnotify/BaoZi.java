c2df4b0ab28980d916b9933e9ab3cc759f8e24ce
jianghao <jianghao19941020@aliyun.com>
2019-08-05 09:48:16 +0800

    线程池、Lambda表达式

---
+++ day-18/src/com/gitee/basics/demo01waitandnotify/BaoZi.java
@@ -0,0 +1,28 @@
+package com.gitee.basics.demo01waitandnotify;
+
+/**
+ * 资源类:包子类
+ * 设置包子的属性
+ * 皮
+ * 陷
+ * 包子的状态: 有 true,没有 false
+ *
+ * @author jianghao
+ * @date 2019/7/29 18:53
+ */
+public class BaoZi {
+
+    /**
+     * 皮
+     */
+    String pi;
+    /**
+     * 陷
+     */
+    String xian;
+    /**
+     * 包子的状态: 有 true,没有 false,设置初始值为false没有包子
+     */
+    boolean flag = false;
+
+}
